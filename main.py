20269ba4934ba7fe93278f36822ed96aa05e5842
Buga Iurii <bugaiurii@yandex.ru>
2022-11-10 17:58:09 +0300

    add exceptions & GET params

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import requests
 
 city = 'city'
+payload = {'MTnq': '', 'lang': 'ru'}
 
-while city != 'end':
+while True:
+
     city = input('Укажите название города: ')
+    if city == 'end':
+        break
+    else:
 
-    url_template = 'http://wttr.in/{name}?MTnq&lang=ru'
-    url = url_template.format(name=city)
+        try:
 
-    response = requests.get(url)
+            url_template = 'http://wttr.in/{name}'
+            url = url_template.format(name=city)
 
-    print(response.text)
+            response = requests.get(url, params=payload)
+            response.raise_for_status()
+
+            print(response.text)
+
+        except:
+            print(None)
     print('Для выхода из программы, введите: end.')
-
-
-
-
-
